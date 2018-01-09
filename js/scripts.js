07c48ce2568503ab3b6c8cca29489934ad0944d6
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-01-09 14:06:30 -0800

    add spaces and semi-colons to scripts.js page

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,20 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle();
-  })
+  });
+
   $(".near").click(function() {
     $(".grover-near").slideDown();
     $(".grover-far").slideUp();
-  })
+  });
+
   $(".far").click(function() {
     $(".grover-near").slideUp();
     $(".grover-far").slideDown();
-  })
+  });
+
   $("#frog").click(function() {
     $("#frog, #frog-phrase").hide();
     $("#prince, #kiss-lyrics").show();
-  })
-})
+  });
+});
