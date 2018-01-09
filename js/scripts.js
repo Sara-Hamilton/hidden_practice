2fe7131a6958ce3868d3d3bc7272d5a0e14d0274
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-01-09 13:59:57 -0800

    add frog to prince functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle();
   })
+  $(".near").click(function() {
+    $(".grover-near").slideDown();
+    $(".grover-far").slideUp();
+  })
+  $(".far").click(function() {
+    $(".grover-near").slideUp();
+    $(".grover-far").slideDown();
+  })
+  $("#frog").click(function() {
+    $("#frog, #frog-phrase").hide();
+    $("#prince, #kiss-lyrics").show();
+  })
 })
